bd68be7c14b4f57aeb70e5d1207b17768f545d8e
jakubklapka <klapka.jacob@icloud.com>
2022-06-09 23:03:43 +0200

    update script.js

--- script.js
+++ script.js
@@ -163,7 +163,7 @@ function what(what, arg1) {
 }
 function getData() {
     var ico = document.getElementById("rejstrikoveico").value;
-    var proxy = "https://mighty-eyrie-00680.herokuapp.com/";
+    var proxy = "https://faktura-proxy.herokuapp.com/";
     var api = "http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=";
     var req = proxy + api + ico;
     return fetch(req)
